72362270ea5a9735f4fbf4500e2759a39b2d7e78
yeseul <yesul0718@naver.com>
2025-06-30 19:38:56 +0900

    redeploy

--- wecam-backend/src/main/java/org/example/wecambackend/WecamBackendApplication.java
+++ wecam-backend/src/main/java/org/example/wecambackend/WecamBackendApplication.java
@@ -19,7 +19,7 @@ public class WecamBackendApplication {
         //local 용
         // .env 파일 로드
         Dotenv dotenv = Dotenv.configure()
-                .directory("./wecam-all-backend") // .env 파일 경로 설정 (기본: 프로젝트 루트)
+                .directory(".") // .env 파일 경로 설정 (기본: 프로젝트 루트)
                 .load();
 
         // 환경변수를 시스템 프로퍼티에 추가
